245fbb1ae380b1e214f503ca77473a9d623aabc5
SALL <62800760+Sallamander-Zerg@users.noreply.github.com>
2020-12-05 11:18:10 +0200

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,38 +1,39 @@
-#include <fcntl.h>
+ #include <fcntl.h>
     #include <sys/stat.h>
     #include <semaphore.h>
     #include <stdio.h>
 
     #define SEM "/my_named_semaphore"
-    void log(FILE *f){
+    #define SEM1 "/my_named_semaphore"
+    void logging(){
+    FILE *f=fopen("text.txt","a");
     fprintf(f, "Logging by process with id %d\n", getpid());
+    fclose(f);
     }
 
     int main(int argc, char ** argv) {
     sem_t *sem;
-m   FILE *f = fopen("text.txt", "a");
-    if ( argc == 2 ) {
-    printf("Dropping semaphore...\n");
-    if ( (sem = sem_open(SEM, 0)) == SEM_FAILED ) {
-   
-    return 1;
-    }
-    sem_post(sem);
+    sem_t *sem1;
     
-    printf("Semaphore dropped.\n");
-    return 0;
-    }
-
-    if ( (sem = sem_open(SEM, O_CREAT, 0777, 0)) == SEM_FAILED ) {
-    log(f);
+    if ( (sem = sem_open(SEM1, O_CREAT, 0777, 1)) == SEM_FAILED ) {
+    logging(f);
     return 1;
     }
-
-    printf("Semaphore is taken.\nWaiting for it to be dropped.\n");
-    if (sem_wait(sem) < 0 )
- 
-    if ( sem_close(sem) < 0 )
-  
+    if ( (sem = sem_open(SEM, O_CREAT, 0777, 0)) == SEM_FAILED ) {
+    logging(f);
+    return 1;
+    }
+	int pid=fork();
+    if(!pid){
+    sem_wait(sem);
+    logging(f);
+    sem_post(sem1);    
+    }else{
+    sem_wait(sem1);
+   logging(f);
+    sem_post(sem);	
+    }
+  sem_unlink(SEM1);
+  sem_unlink(SEM);
     return 0;
     }
-
